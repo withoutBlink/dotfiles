0b905643611065a1974804d9b88650090d5955a3
qlyang <yeluoqiulin@gmail.com>
2024-07-14 21:21:59 +0800

    Fix(fish): seperate macos config from linux

--- fish/config.fish
+++ fish/config.fish
@@ -2,12 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 
 	fish_vi_key_bindings
-	set -g JAVA_HOME '/opt/homebrew/Cellar/openjdk@17/17.0.7/libexec/openjdk.jdk/Contents/Home'
+	# set -g JAVA_HOME '/opt/homebrew/Cellar/openjdk@17/17.0.7/libexec/openjdk.jdk/Contents/Home'
 
 	alias ls "ls -F"
 	alias ll "exa --long --header --git -F --icons"
-	alias strace "dtruss"
-	alias ldd "otool -L"
 	alias ipa "ifconfig en0"
 
 	# git short-cut
@@ -22,9 +20,6 @@ if status is-interactive
 	set fish_color_normal brcyan
 	set fish_color_command brcyan
 	set -gx LS_COLORS "di=32" 
-
-	set -gx HOMEBREW_NO_AUTO_UPDATE 1
-	set -gx ANDROID_SDK "/Users/andrew/Library/Android/sdk"
 
 	set -gx ALL_PROXY "http://127.0.0.1:10807"
 	set -gx all_proxy "http://127.0.0.1:10807"
